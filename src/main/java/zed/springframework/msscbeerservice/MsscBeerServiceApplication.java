6bd37f2f9286320de7a1a4b0b7c9c1987e044749
ashutosh.ac.singh <zebracross2007@gmail.com>
2020-04-04 15:30:37 +0530

    beer model

--- src/main/java/zed/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/zed/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -8,6 +8,7 @@ public class MsscBeerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBeerServiceApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/zed/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package zed.springframework.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE , PORTER, ALE , WHEAT, IPA
+}
